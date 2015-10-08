a0ad0a7352780c00db773cb42040ffcadd499afe
Sem <Sem@LAP-SEM>
2015-10-08 09:47:42 +0200

    Removed the run of cat.bfck

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,6 @@ public class Main {
 	public static void main(String[] args) {
 		BFCKinterpreter helloWorld = new BFCKinterpreter("HelloWorld.bfck");
 		helloWorld.run();
-		
-		BFCKinterpreter cat = new BFCKinterpreter("cat.bfck");
-		//cat.run();
-		
-		
 	}
 
 }
